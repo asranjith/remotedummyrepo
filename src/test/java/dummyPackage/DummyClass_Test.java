e2f0fefb506f9a06ab7802e4a78b21c53c44dd45
asranjith <rockyranjith27@gmail.com>
2024-11-11 15:27:39 +0530

    changes commit

--- src/test/java/dummyPackage/DummyClass_Test.java
+++ src/test/java/dummyPackage/DummyClass_Test.java
@@ -1,5 +1,10 @@
 package dummyPackage;
 
-public class DummyClass_Test {
+import org.testng.annotations.Test;
 
+public class DummyClass_Test {
+    @Test
+    public void dummyTest() {
+    	System.out.println("my changes");
+    }
 }
